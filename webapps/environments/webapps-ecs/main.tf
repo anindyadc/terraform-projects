2fcbd06be3a911b71434ac9ec998aec00d6c14ee
anindyadc <anindyadc@gmail.com>
2025-06-23 22:37:03 +0530

    delete webapps project

--- webapps/environments/webapps-ecs/main.tf
+++
@@ -1,7 +0,0 @@
-module "webapp_ecs" {
-  source          = "../../modules/webapp_ecs"
-  name            = "my-webapp"
-  image           = "123456789.dkr.ecr.us-east-1.amazonaws.com/webapp:v1"
-  container_port  = 8080
-  region          = "us-east-1"
-}
